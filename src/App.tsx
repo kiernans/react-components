b17d457022a7e6aae2a88cbae261fc2a13b0ae3b
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-02-12 16:05:48 -0700

    Create Button component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import Button from './components/Button';
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+
 function App() {
-	return <div className='App'>App</div>;
+	return (
+		<div className='App'>
+			<div>
+				<Button primary rounded>
+					<GoBell />
+					Click!
+				</Button>
+			</div>
+			<div>
+				<Button success rounded outline>
+					<GoCloudDownload />
+					Buy Now!
+				</Button>
+			</div>
+			<div>
+				<Button warning>
+					<GoDatabase />
+					See Deal!
+				</Button>
+			</div>
+			<div>
+				<Button danger>Click</Button>
+			</div>
+			<div>
+				<Button secondary outline>
+					Click
+				</Button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,78 @@
-import React from 'react';
+import { ButtonHTMLAttributes } from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import PropTypes from 'prop-types';
+import className from 'classnames';
 
-const Button = () => {
-	return <button>Submit</button>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	children?: React.ReactNode;
+	primary?: boolean;
+	secondary?: boolean;
+	success?: boolean;
+	warning?: boolean;
+	danger?: boolean;
+	outline?: boolean;
+	rounded?: boolean;
+};
+
+const Button = ({
+	children,
+	primary,
+	secondary,
+	success,
+	warning,
+	danger,
+	outline,
+	rounded,
+	...rest
+}: ButtonProps) => {
+	const classes = className(
+		rest.className,
+		'flex items-center px-3 py-1.5 border',
+		{
+			'border-blue-500 bg-blue-500': primary,
+			'border-gray-900 bg-gray-900': secondary,
+			'border-green-500 bg-green-500': success,
+			'border-yellow-400 bg-yellow-400': warning,
+			'border-red-500 bg-red-500': danger,
+			'rounded-full': rounded,
+			'text-white':
+				!outline && (primary || secondary || success || warning || danger),
+			'bg-white': outline,
+			'text-blue-500': outline && primary,
+			'text-gray-500': outline && secondary,
+			'text-green-500': outline && success,
+			'text-yellow-500': outline && warning,
+			'text-red-500': outline && danger,
+		}
+	);
+
+	return (
+		<button {...rest} className={classes}>
+			{children}
+		</button>
+	);
+};
+
+Button.propTypes = {
+	checkVariationValue: ({
+		primary,
+		secondary,
+		success,
+		warning,
+		danger,
+	}: ButtonProps) => {
+		const count =
+			Number(!!primary) +
+			Number(!!secondary) +
+			Number(!!warning) +
+			Number(!!success) +
+			Number(!!danger);
+		if (count > 1) {
+			return new Error(
+				'Only one of primary, secondary, warning, success, danger can be true'
+			);
+		}
+	},
 };
 
 export default Button;
