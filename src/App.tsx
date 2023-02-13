b0e8b5d24a98179d98b97f105485f8ea15483513
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-02-12 21:37:02 -0700

    Create DropdownPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import { useState } from 'react';
-import Dropdown from './components/Dropdown';
-import { Option } from './components/Dropdown';
+import DropdownPage from './pages/DropdownPage';
 
 function App() {
-	const [selection, setSelection] = useState<Option | null>(null);
-
-	const handleSelection = (option: Option) => {
-		setSelection(option);
-	};
-
-	const options = [
-		{ label: 'Red', value: 'red' },
-		{ label: 'Green', value: 'green' },
-		{ label: 'Blue', value: 'blue' },
-	];
-
 	return (
-		<div className='flex'>
-			<Dropdown
-				options={options}
-				value={selection}
-				onChange={handleSelection}
-			/>
-			<Dropdown
-				options={options}
-				value={selection}
-				onChange={handleSelection}
-			/>
+		<div className='App'>
+			<DropdownPage />
 		</div>
 	);
 }
---
+++ src/pages/DropdownPage.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Dropdown from '../components/Dropdown';
+import { Option } from '../components/Dropdown';
+
+function DropdownPage() {
+	const [selection, setSelection] = useState<Option | null>(null);
+
+	const handleSelection = (option: Option) => {
+		setSelection(option);
+	};
+
+	const options = [
+		{ label: 'Red', value: 'red' },
+		{ label: 'Green', value: 'green' },
+		{ label: 'Blue', value: 'blue' },
+	];
+
+	return (
+		<div className='flex'>
+			<Dropdown
+				options={options}
+				value={selection}
+				onChange={handleSelection}
+			/>
+			<Dropdown
+				options={options}
+				value={selection}
+				onChange={handleSelection}
+			/>
+		</div>
+	);
+}
+
+export default DropdownPage;
